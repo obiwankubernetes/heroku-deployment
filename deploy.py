28583f8237c013be9541bd09114df3bf3541518a
obiwankubernetes <samrperl@gmail.com>
2020-06-05 23:38:15 -0400

    1st

---
+++ deploy.py
@@ -0,0 +1,15 @@
+### deploy to heroku
+# install heroku cli from web
+# ensure heroku in env path -> heroku --version
+# login -> heroku login
+# make procfile
+# make setup.sh file
+# potentially create .gitignore file for venv and others
+# init git -> git init
+# add all code to git -> git add 
+# push to github repo
+# create app in heroku -> heroku create <appname>
+# align git repo with heroku -> heroku git:remote -a photo-enhancer-webapp
+# git add .
+# git commit -am "to heroku"
+# git push heroku master
